fb8b366887be5cc0fab15ddb51e1fa653e2c9476
charlene-j <148769967+charlene-j@users.noreply.github.com>
2026-09-25 11:14:04 +0200

    Update doctor.rs
    update to merge the sanitized data in one csv file

--- src/doctor.rs
+++ src/doctor.rs
@@ -39,28 +39,68 @@ pub fn doctor_keys_generation(path_doctor_public_key: &PathBuf, path_doctor_priv
     print!("The private/public keys are correctly generated and stored ({}/{} bytes).\n", privk_size, pubk_size);
 }
 
+// Preprocessing of the data.
+pub fn doctor_preprocessing(path_data: &String)->(usize,usize,Vec<Metadata>){
+    // Open the CSV file.
+    let mut reader = csv::Reader::from_path(&path_data).expect("Cannot open the document");
+    // Collect the headers.
+    let headers = reader.headers().expect("Cannot read the headers").clone();
+
+    // Collect the number of columns.
+    let column_number: usize = headers.len();
+
+    // Collect the number of rows.
+    let row_number: usize = reader.records().count();
+
+    // Define metadata containing the type of data, the precision, the range...
+    let mut dataset_metadata: Vec<Metadata> = Vec::with_capacity(row_number);
+
+    for column_index in 0..column_number{  
+        let name_data = &headers[column_index];
+
+        // Since we work with integer, the values are discretized.
+        // For example for the serum_creatinine, the value 1.9 will be changed in 19. 
+        // (After randomization, the anonymized dataset preserves the floating values).
+        let pr = get_precision(&path_data, column_index);
+
+        // Computing the max data in the dataset allows to determine a range for the anonymized data. 
+        // Notice that this range determine the maximum value of data that can be taken after randomization. 
+        // The range should be fixed by a knowledge expert depending on the type of data (it should not depending on the value of the dataset) 
+        // For example for the patient age, it is suitable to fixe max_data = 100, subsequently l_2 = 128).
+        // The range can also be taken larger to ensure more privacy.
+        let max_data = compute_max(&path_data, column_index, row_number, pr); 
+
+        // l2 corresponds to the ceiling of the base-2 logarithm of max_data. l2 >= log_2(max_data).
+        let l_2 = compute_l2(max_data); 
+
+        dataset_metadata.push(Metadata{type_data: name_data.to_string(), col_index: column_index, precision: pr, l2: l_2});
+    }
+    return (column_number, row_number, dataset_metadata);
+}
+
 // Computation of the parameter l1 according to the privacy budget.
-pub fn doctor_precomputation(name_data: &String, row_index: usize, max_eps: f32, l2: u32, path_eps: &PathBuf) -> u32{ // b = 0 for integer and b = 1 for flot; p is the precision in the case b = 1. The precision corresponds to the number after the comma. For instance, if we have age = 20.264, p = 3.
-	
+pub fn doctor_precomputation(name_data: &String, column_index: usize, row_index: usize, max_eps: f64, l_2: u32, path_eps: &String) -> u32{ // The precision corresponds to the number after the comma. For instance, if we have age = 20.264, pr = 3.
+
     // Compute the parameter l1 
-    let l1 = compute_l1(l2, max_eps);
+    let l_1 = compute_l1(l_2, max_eps);
 
     // Compute the exact privacy budget
-    let eps = compute_eps(l2, l1);
-    let _= store_float(eps, &path_eps);
+    let eps = compute_eps(l_2, l_1);
+    //let _= store_float(eps, &path_eps);
+    let _ = write_csv_file(path_eps, column_index, row_index, &eps.to_string()); 
 	
     print!("Patient: {}; data: {}, eps: {}.\n", row_index, name_data, eps);
 	
-    return l1
+    return l_1
 }
 
 // Generation and Storage of the setup
-pub fn doctor_setup_generation_and_storage(l1: u32, l2: u32, path_set: &PathBuf){
+pub fn doctor_setup_generation_and_storage(l_1: u32, l_2: u32, path_set: &PathBuf){
 
     let mut csrng = OsRng; // Random Number Generator based on the OS System (Crypto-Secure RNG)
     
     // Setup Operation
-    let set = setup(&mut csrng, l1.try_into().unwrap(), l2.try_into().unwrap()); 
+    let set = setup(&mut csrng, l_1.try_into().unwrap(), l_2.try_into().unwrap()); 
     
     // Store set
     let _ = store_setup(&set, &path_set);
@@ -75,24 +115,24 @@ pub fn doctor_setup_generation_and_storage(l1: u32, l2: u32, path_set: &PathBuf)
 }
 
 // Generation and Storage of the opening key, the commitment and the proof of correct commitment.
-pub fn doctor_commit_generation_and_storage(column_index: usize, row_index: usize, l1: u32, l2: u32, b: bool, p: u32, path_data: &String, path_set: &PathBuf, path_opening_key: &PathBuf, path_commitment: &PathBuf, path_proof_commitment: &PathBuf){
+pub fn doctor_commit_generation_and_storage(column_index: usize, row_index: usize, l_1: u32, l_2: u32, pr: u32, path_data: &String, path_set: &PathBuf, path_opening_key: &PathBuf, path_commitment: &PathBuf, path_proof_commitment: &PathBuf){
 
     let mut csrng = OsRng; // Random Number Generator based on the OS System (Crypto-Secure RNG)
     
     // Import the data
     let data: u32;
-    if b == false{
-	data = import_data_u32(&path_data, column_index, row_index).expect("Impossible to import the data.");
+    if pr == 0{
+	   data = import_data_u32(&path_data, column_index, row_index).expect("Impossible to import the data.");
     }
     else{ 
-	data = import_data_f32(&path_data, column_index, row_index, p).expect("Impossible to import the data.");
+	   data = import_data_f64(&path_data, column_index, row_index, pr).expect("Impossible to import the data.");
     }
 	
     // Convert the data in binary 
-    let bin_data = convert_u32_in_binary(data, l2.try_into().unwrap());
+    let bin_data = convert_u32_in_binary(data, l_2.try_into().unwrap());
     
     // Choose a random seed theta
-    let theta = vec![rand_bitstring(l1.try_into().unwrap()), rand_bitstring(l2.try_into().unwrap())];
+    let theta = vec![rand_bitstring(l_1.try_into().unwrap()), rand_bitstring(l_2.try_into().unwrap())];
     
     // Extract the setup
     let set = extract_setup(&path_set);
@@ -121,7 +161,7 @@ pub fn doctor_commit_generation_and_storage(column_index: usize, row_index: usiz
     let _ = store_commit(&c, &path_commitment); 
         
     // Verify that we can extract the commitment from the file
-    let extracted_c = extract_commit(l1.try_into().unwrap(), l2.try_into().unwrap(), &path_commitment); // Collect the stored commitment 
+    let extracted_c = extract_commit(l_1.try_into().unwrap(), l_2.try_into().unwrap(), &path_commitment); // Collect the stored commitment 
     assert!(extracted_c == c, "The commitment in the file in not equal to the real commitment."); // Verify that the real commitment and the collected commitment are the same
     
     let meta_com = fs::metadata(&path_commitment).expect("Error");
@@ -129,14 +169,14 @@ pub fn doctor_commit_generation_and_storage(column_index: usize, row_index: usiz
     print!("The commitment is generated and stored ({} bytes).\n", com_size);
     	
     // Store the proof of commitment
-    let _ = store_proofcommit(&p_com, l1.try_into().unwrap(), l2.try_into().unwrap(), &path_proof_commitment);
+    let _ = store_proofcommit(&p_com, l_1.try_into().unwrap(), l_2.try_into().unwrap(), &path_proof_commitment);
     
     let meta_pcom = fs::metadata(&path_proof_commitment).expect("Error");
     let pcom_size = meta_pcom.len();
     print!("The proof of commitment is generated and stored ({} bytes).\n", pcom_size); 
  
     // Verify that we can extract the proof of commitment from the file
-    let extracted_pcom = extract_proofcommit(l1.try_into().unwrap(), l2.try_into().unwrap(), &path_proof_commitment); // Collect the stored proof of commitment
+    let extracted_pcom = extract_proofcommit(l_1.try_into().unwrap(), l_2.try_into().unwrap(), &path_proof_commitment); // Collect the stored proof of commitment
     assert!(extracted_pcom == p_com, "The proof of commitment in the file in not equal to the real proof of commitment."); // Verify that the real proof of commitment and the collected proof of commitment are the same
 }   	
 
@@ -148,11 +188,11 @@ pub fn doctor_signature_generation_and_storage(path_set: &PathBuf, path_commitme
     // Extract the setup from the file
     let set = extract_setup(&path_set);
     
-    let l1 = set.l1;
-    let l2 = set.l2;
+    let l_1 = set.l1;
+    let l_2 = set.l2;
    
     // Extract the commitment from the file
-    let commitment = extract_commit(l1, l2, &path_commitment);
+    let commitment = extract_commit(l_1, l_2, &path_commitment);
    
     // Sign phase:
     // Extract the public and private key from the file
@@ -175,52 +215,79 @@ pub fn doctor_signature_generation_and_storage(path_set: &PathBuf, path_commitme
 }
 
 // Phase executed by the Doctor.
-pub fn doctor_phase(metadata: &Metadata, max_eps: Vec<f32>, path_data: &String, folder_eps: &String, folder_set: &String, folder_opening_key: &String, folder_commitment: &String, folder_proof_commitment: &String, folder_signature: &String, path_doctor_public_key: &PathBuf, path_doctor_private_key: &PathBuf){
+pub fn doctor_phase(column_number: usize, row_number: usize, dataset_metadata: &Vec<Metadata>, path_data: &String, path_privacy: &String, path_eps: &String, folder_set: &String, folder_opening_key: &String, folder_commitment: &String, folder_proof_commitment: &String, folder_signature: &String, path_doctor_public_key: &PathBuf, path_doctor_private_key: &PathBuf){
 
     let start_doc_phase = Instant::now();
 
-    // Parse metadata
-    let name_data = &metadata.name;
-    let column_index = metadata.col_index;
-    let number_row = metadata.nb_row;
-    let b = metadata.fl;
-    let p = metadata.pr;
-    let l2 = metadata.l2;
+    let mut reader_privacy = csv::Reader::from_path(&path_privacy).expect("Cannot open the document");
 
-    let folder_eps_string = format!("{}/{}", folder_eps, name_data);
-    let folder_eps = Path::new(&folder_eps_string);
-    let folder_set_string = format!("{}/{}", folder_set, name_data);
-    let folder_set = Path::new(&folder_set_string);
-    let folder_opening_key_string = format!("{}/{}", folder_opening_key, name_data);
-    let folder_opening_key = Path::new(&folder_opening_key_string);
-    let folder_commitment_string = format!("{}/{}", folder_commitment, name_data);
-    let folder_commitment = Path::new(&folder_commitment_string);
-    let folder_proof_commitment_string = format!("{}/{}", folder_proof_commitment, name_data);
-    let folder_proof_commitment = Path::new(&folder_proof_commitment_string);
-    let folder_signature_string = format!("{}/{}", folder_signature, name_data);
-    let folder_signature = Path::new(&folder_signature_string);
-    let _ = fs::create_dir_all(folder_eps); // Create the folder of privacy budget.
-    let _ = fs::create_dir_all(folder_set); // Create the folder of set.
-    let _ = fs::create_dir_all(folder_opening_key); // Create the folder of the opening_key.
-    let _ = fs::create_dir_all(folder_commitment); // Create the folder of the commitment.
-    let _ = fs::create_dir_all(folder_proof_commitment); // Create the folder of the proof of commitment.
-    let _ = fs::create_dir_all(folder_signature); // Create the folder of the signature.
+    let privacy_attending: Vec<Vec<String>> = reader_privacy.records().map(|result| {result.expect("Error reading CSV").iter().map(|s| s.to_string()).collect()}).collect();
 
-    for row_index in 1..=number_row{	
-        let path_eps = folder_eps.join(format!("eps_{}.txt", row_index)); 
-        let path_set = folder_set.join(format!("set_{}.txt", row_index)); 
-	let path_opening_key = folder_opening_key.join(format!("opening_key_{}.txt", row_index)); 
-	let path_commitment = folder_commitment.join(format!("commitment_{}.txt", row_index));
-	let path_proof_commitment = folder_proof_commitment.join(format!("proof_commitment_{}.txt", row_index));
-	let path_signature = folder_signature.join(format!("signature_{}.txt", row_index));
+    // Define some privacy budget (the lower the value, the better the confidentiality, but the lower the utility).
+    let eps_highly_sensitive: f64 = 0.5;
+    let eps_sensitive: f64 = 0.75;
+    let eps_less_sensitive: f64 = 1.2;
+
+    // Create a file to store the privacy budget
+    let _= init_csv_file(column_number, row_number, dataset_metadata, path_eps);
+
+    for column_index in 0..column_number{
+        // Parse metadata
+        let name_data = dataset_metadata[column_index].type_data.clone();
+        let pr = dataset_metadata[column_index].precision.clone();
+        let l_2 = dataset_metadata[column_index].l2.clone();
+
+        let folder_set_string = format!("{}/{}", folder_set, name_data);
+        let folder_set = Path::new(&folder_set_string);
+        let folder_opening_key_string = format!("{}/{}", folder_opening_key, name_data);
+        let folder_opening_key = Path::new(&folder_opening_key_string);
+        let folder_commitment_string = format!("{}/{}", folder_commitment, name_data);
+        let folder_commitment = Path::new(&folder_commitment_string);
+        let folder_proof_commitment_string = format!("{}/{}", folder_proof_commitment, name_data);
+        let folder_proof_commitment = Path::new(&folder_proof_commitment_string);
+        let folder_signature_string = format!("{}/{}", folder_signature, name_data);
+        let folder_signature = Path::new(&folder_signature_string);
+        let _ = fs::create_dir_all(folder_set); // Create the folder of set.
+        let _ = fs::create_dir_all(folder_opening_key); // Create the folder of the opening_key.
+        let _ = fs::create_dir_all(folder_commitment); // Create the folder of the commitment.
+        let _ = fs::create_dir_all(folder_proof_commitment); // Create the folder of the proof of commitment.
+        let _ = fs::create_dir_all(folder_signature); // Create the folder of the signature.
+
+        for row_index in 1..=row_number{
+            let path_set = folder_set.join(format!("set_{}.txt", row_index)); 	 
+	        let path_opening_key = folder_opening_key.join(format!("opening_key_{}.txt", row_index)); 
+	        let path_commitment = folder_commitment.join(format!("commitment_{}.txt", row_index));
+	        let path_proof_commitment = folder_proof_commitment.join(format!("proof_commitment_{}.txt", row_index));
+	        let path_signature = folder_signature.join(format!("signature_{}.txt", row_index));
+
+            let max_eps: f64;
+
+            if privacy_attending[row_index-1][column_index] == "high"{
+                max_eps = eps_highly_sensitive;
+            }
+            else{
+                if privacy_attending[row_index-1][column_index] == "moderate"{
+                    max_eps = eps_sensitive;
+                }
+                else{
+                    if privacy_attending[row_index-1][column_index] == "low"{
+                        max_eps = eps_less_sensitive; 
+                    }
+                    else{ // none
+                        max_eps = 0f64;
+                    }
+                }
+            }
+
+	        let l_1 = doctor_precomputation(&name_data, column_index, row_index, max_eps, l_2, path_eps);
 		
-	let l1 = doctor_precomputation(&name_data, row_index, max_eps[row_index-1], l2, &path_eps);
-		
-	doctor_setup_generation_and_storage(l1, l2, &path_set);
-	doctor_commit_generation_and_storage(column_index, row_index, l1, l2, b, p, &path_data, &path_set, &path_opening_key, &path_commitment, &path_proof_commitment);
-	doctor_signature_generation_and_storage(&path_set, &path_commitment, &path_signature, &path_doctor_public_key, &path_doctor_private_key);	
+	        doctor_setup_generation_and_storage(l_1, l_2, &path_set);
+	        doctor_commit_generation_and_storage(column_index, row_index, l_1, l_2, pr, &path_data, &path_set, &path_opening_key, &path_commitment, &path_proof_commitment);
+	        doctor_signature_generation_and_storage(&path_set, &path_commitment, &path_signature, &path_doctor_public_key, &path_doctor_private_key);	
+        }
     }
     let duration_doc_phase = start_doc_phase.elapsed();
-    print!("Doctor Phase took {:?} for {} data \n", duration_doc_phase, number_row);
+    print!("Doctor Phase took {:?} for {} data \n", duration_doc_phase, row_number);
 }
+	
 	
